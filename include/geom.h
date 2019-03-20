dceaddee671dd3d35e5acb4f0b0279b563d7ef6a
Paolo Lammens <lammenspaolo@gmail.com>
2019-03-20 16:04:04 +0100

    feat: geom utils

---
+++ include/geom.h
@@ -0,0 +1,32 @@
+#ifndef CONVEXPOLYGONS_GEOM_H
+#define CONVEXPOLYGONS_GEOM_H
+
+#include "class/Point.h"
+
+// Represents a segment as a line Ax + By = C delimited by two points
+class Segment {
+    Point _start, _end;
+    double _A, _B, _C;
+
+public:
+    Segment(const Point &, const Point &);
+    double A() const { return _A; }
+    double B() const { return _B; }
+    double C() const { return _C; }
+    const Point &start() const { return _start; }
+    const Point &end() const { return _end; }
+};
+
+
+struct SolveResult {
+    Point result;
+    bool success;
+};
+
+
+SolveResult intersect(const Segment &r, const Segment &s);
+
+bool isInSegment(const Point &P, const Segment &r);
+
+
+#endif //CONVEXPOLYGONS_GEOM_H
---
+++ src/geom.cc
@@ -0,0 +1,33 @@
+//
+
+#include <geom.h>
+
+#include "geom.h"
+#include "utils.h"
+
+
+SolveResult intersect(const Segment &r, const Segment &s) {
+    double det = r.A()*s.B() - r.B()*s.A();
+    if (numericEquals(det, 0)) return {{}, false};
+
+    // Solve by Cramer's rule:
+    Point intersection{};
+    intersection.x = (r.C()*s.B() - r.B()*s.C())/det;
+    intersection.y = (r.A()*s.C() - r.C()*s.A())/det;
+
+    bool withinBounds = isInSegment(intersection, r) and isInSegment(intersection, s);
+    return {intersection, withinBounds};
+}
+
+
+bool isInSegment(const Point &P, const Segment &r) {
+    Vector2D direction = r.end() - r.start(), u = P - r.start();
+    if (u.x*direction.y != u.y*direction.x) return false;
+    double lambda = (numericEquals(direction.x, 0) ? u.y/direction.y : u.x/direction.x);
+    return 0 <= lambda and lambda <= 1;
+}
+
+
+Segment::Segment(const Point &start, const Point &end) : _start(start), _end(end),
+                                                         _A(end.y - start.y), _B(start.x - end.x),
+                                                         _C(_A*start.x + _B*start.y) {}
