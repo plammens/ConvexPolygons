282bb96ab48e1285e4b7f2b3d813dd43293b48c0
Paolo Lammens <lammenspaolo@gmail.com>
2019-03-20 14:04:44 +0100

    refactor: const char * to auto

--- include/errors.h
+++ include/errors.h
@@ -32,35 +32,35 @@ class Error : public virtual BaseException {};
 
 class UnknownCommand : public Error {
 public:
-    static constexpr const char *base = "unrecognized command";
+    static constexpr auto base = "unrecognized command";
     UnknownCommand(const string &specific = "") : BaseException(base, specific) {}
 };
 
 
 class SyntaxError : public Error {
 public:
-    static constexpr const char *base = "invalid command syntax";
+    static constexpr auto base = "invalid command syntax";
     SyntaxError(const string &specific = "") : BaseException(base, specific) {}
 };
 
 
 class ValueError : public Error {
 public:
-    static constexpr const char *base = "invalid value";
+    static constexpr auto base = "invalid value";
     ValueError(const string &specific = "") : BaseException(base, specific) {}
 };
 
 
 class UndefinedID : public Error {
 public:
-    static constexpr const char *base = "undefined ID";
+    static constexpr auto base = "undefined ID";
     UndefinedID(const string &specific = "") : BaseException(base, specific) {}
 };
 
 
 class IOError : public Error {
 public:
-    static constexpr const char *base = "unable to access file";
+    static constexpr auto base = "unable to access file";
     IOError(const string &specific = "") : BaseException(base, specific) {}
 };
 
@@ -73,7 +73,7 @@ class Warning : public virtual BaseException {};
 
 class UnusedArgument : public Warning {
 public:
-    static constexpr const char *base = "unused argument(s)";
+    static constexpr auto base = "unused argument(s)";
     UnusedArgument(const string &specific = "") : BaseException(base, specific) {}
 };
 
