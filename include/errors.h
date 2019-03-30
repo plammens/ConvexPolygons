ecd8b8640081947fdb817071ad5b86ce819ad60b
Paolo Lammens <lammenspaolo@gmail.com>
2019-03-30 17:33:00 +0100

    docs: document errors

--- include/errors.h
+++ include/errors.h
@@ -1,5 +1,8 @@
-// Custom exceptions for the whole project
-// (header-only)
+/**
+ * @file
+ * Custom exceptions for the whole project. Exceptions are grouped into
+ * errors and warnings, both of which inherit from a custom base exception.
+ */
 
 #ifndef CONVEXPOLYGONS_ERRORS_H
 #define CONVEXPOLYGONS_ERRORS_H
@@ -8,28 +11,49 @@
 #include <sstream>
 
 
+//-------- BASE EXCEPTION --------//
+
+/**
+ * Base class for all custom exceptions. Stores a message as a string.
+ * Instances can only be constructed from within a child class,
+ * since the constructors are protected.
+ */
 class BaseException : public std::exception {
 protected:
-    std::string message;
+    std::string message;  ///< message to be returned by what()
 
     BaseException() = default;
-    BaseException(const std::string &baseMessage, const std::string &specific = "") {
+
+    /**
+     * Construct a new exception from a base message and optional additional details.
+     * The base message is intended to be class-specific, while the additional
+     * details string is intended to be instance-specific.
+     *
+     * @param baseMessage  generic message for the kind of exception being created
+     * @param details  additional information as to why the exception was thrown
+     */
+    BaseException(const std::string &baseMessage, const std::string &details = "") {
         std::ostringstream oss(baseMessage, std::ios::ate);
-        if (not specific.empty()) oss << " (" << specific << ')';
+        if (not details.empty()) oss << " (" << details << ')';
         message = oss.str();
     }
 
 public:
+    /// `std::exception`-compliant message getter
     const char *what() const noexcept override {
         return message.c_str();
     }
 };
 
-// ---------------- Errors -----------------------
 
+
+//-------- ERRORS --------//
+
+/// virtual base class from which all errors inherit
 class Error : public virtual BaseException {};
 
 
+/// Indicates that a command whose keyword is unknown was issued
 class UnknownCommand : public Error {
 public:
     static constexpr auto base = "unrecognized command";
@@ -37,6 +61,7 @@ public:
 };
 
 
+/// Indicates that a certain command was issued with an invalid syntax
 class SyntaxError : public Error {
 public:
     static constexpr auto base = "invalid command syntax";
@@ -44,6 +69,8 @@ public:
 };
 
 
+/// Indicates that an algorithm encountered a situation in which it is not well-defined;
+/// i.e., a value that doesn't meet a function's prerequisites was passed.
 class ValueError : public Error {
 public:
     static constexpr auto base = "invalid value";
@@ -51,6 +78,7 @@ public:
 };
 
 
+/// Indicates that a certain polygon ID doesn't yet exist
 class UndefinedID : public Error {
 public:
     static constexpr auto base = "undefined ID";
@@ -58,6 +86,7 @@ public:
 };
 
 
+/// Indicates an error in reading or writing to a file
 class IOError : public Error {
 public:
     static constexpr auto base = "unable to access file";
@@ -65,12 +94,15 @@ public:
 };
 
 
-// -------------------- Warnings ----------------------
 
 
+//-------- WARNINGS --------//
+
+/// virtual base class from which all warnings inherit
 class Warning : public virtual BaseException {};
 
 
+/// Indicates that an unused argument was detected when parsing a command
 class UnusedArgument : public Warning {
 public:
     static constexpr auto base = "unused argument(s)";
