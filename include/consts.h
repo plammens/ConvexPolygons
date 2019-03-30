4ff7d390cf884a2172c6639ff23efea3b7c9d77b
Paolo Lammens <lammenspaolo@gmail.com>
2019-03-30 19:21:51 +0100

    docs: documented consts

--- include/consts.h
+++ include/consts.h
@@ -49,20 +49,25 @@ namespace io {
  */
 namespace img {
 
-    constexpr struct { int X, Y; } SIZE{500, 500};
-    constexpr int PADDING = 2;  // padding in pixels
-    constexpr double BACKGROUND = 1.0;  // background color
-    constexpr double POL_OPACITY = 0.6;  // polygon opacity
+    constexpr struct { int X, Y; } SIZE = {500, 500};  ///< image size in pixels, (`width`, `height`)
+    constexpr struct { int X, Y; } CENTER = {(SIZE.X + 1)/2, (SIZE.Y + 1)/2};
+    ///< deduced center of image, in (x, y) coordinates
 
-    constexpr struct { int X, Y; } CENTER{(SIZE.X + 1)/2, (SIZE.Y + 1)/2};
-    constexpr struct { int X, Y; } DRAW_SIZE{SIZE.X - 2*PADDING, SIZE.Y - 2*PADDING};
+    constexpr int PADDING = 2;  ///< padding in pixels
+    constexpr struct { int X, Y; } DRAW_SIZE = {SIZE.X - 2*PADDING, SIZE.Y - 2*PADDING};
+    ///< deduced size of available drawing space (taking padding into account)
+
+    constexpr double BACKGROUND = 1.0;  ///< background color, in the grayscale range [0, 1]
+    constexpr double POL_OPACITY = 0.6;  ///< polygon opacity for filled drawings
 
 }
 
 
 /// Namespace for anything related to numerical computations
 namespace numeric {
-    constexpr double EPSILON = 1e-12; 
+
+    constexpr double EPSILON = 1e-12; ///< absolute numerical tolerance constant
+
 }
 
 
